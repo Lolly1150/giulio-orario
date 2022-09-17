8bcddd053cf3f84e4c651d660e71ea779a8b5c6d
Lolly1150 <lorenzodercoli09@gmail.com>
2022-09-17 18:00:41 +0200

    Update

---
+++ src/interfaces/calendar.ts
@@ -0,0 +1,184 @@
+export interface calendarInterface {
+  1: {
+    1: {
+      prof: string;
+      subject: string;
+    };
+    2: {
+      prof: string;
+      subject: string;
+    };
+    3: {
+      prof: string;
+      subject: string;
+    };
+    4: {
+      prof: string;
+      subject: string;
+    };
+    5: {
+      prof: string;
+      subject: string;
+    };
+    6: {
+      prof: string;
+      subject: string;
+    };
+  };
+  2: {
+    1: {
+      prof: string;
+      subject: string;
+    };
+    2: {
+      prof: string;
+      subject: string;
+    };
+    3: {
+      prof: string;
+      subject: string;
+    };
+    4: {
+      prof: string;
+      subject: string;
+    };
+    5: {
+      prof: string;
+      subject: string;
+    };
+    6: {
+      prof: string;
+      subject: string;
+    };
+  };
+  3: {
+    1: {
+      prof: string;
+      subject: string;
+    };
+    2: {
+      prof: string;
+      subject: string;
+    };
+    3: {
+      prof: string;
+      subject: string;
+    };
+    4: {
+      prof: string;
+      subject: string;
+    };
+    5: {
+      prof: string;
+      subject: string;
+    };
+    6: {
+      prof: string;
+      subject: string;
+    };
+  };
+  4: {
+    1: {
+      prof: string;
+      subject: string;
+    };
+    2: {
+      prof: string;
+      subject: string;
+    };
+    3: {
+      prof: string;
+      subject: string;
+    };
+    4: {
+      prof: string;
+      subject: string;
+    };
+    5: {
+      prof: string;
+      subject: string;
+    };
+    6: {
+      prof: string;
+      subject: string;
+    };
+  };
+  5: {
+    1: {
+      prof: string;
+      subject: string;
+    };
+    2: {
+      prof: string;
+      subject: string;
+    };
+    3: {
+      prof: string;
+      subject: string;
+    };
+    4: {
+      prof: string;
+      subject: string;
+    };
+    5: {
+      prof: string;
+      subject: string;
+    };
+    6: {
+      prof: string;
+      subject: string;
+    };
+  };
+  6: {
+    1: {
+      prof: string;
+      subject: string;
+    };
+    2: {
+      prof: string;
+      subject: string;
+    };
+    3: {
+      prof: string;
+      subject: string;
+    };
+    4: {
+      prof: string;
+      subject: string;
+    };
+    5: {
+      prof: string;
+      subject: string;
+    };
+    6: {
+      prof: string;
+      subject: string;
+    };
+  };
+  7: {
+    1: {
+      prof: string;
+      subject: string;
+    };
+    2: {
+      prof: string;
+      subject: string;
+    };
+    3: {
+      prof: string;
+      subject: string;
+    };
+    4: {
+      prof: string;
+      subject: string;
+    };
+    5: {
+      prof: string;
+      subject: string;
+    };
+    6: {
+      prof: string;
+      subject: string;
+    };
+  };
+}
---
+++ src/orario.ts
@@ -0,0 +1,188 @@
+import type { calendarInterface } from "./interfaces";
+
+const calendar: object = {
+  1: {
+    1: {
+      prof: "",
+      subject: "",
+    },
+    2: {
+      prof: "",
+      subject: "",
+    },
+    3: {
+      prof: "",
+      subject: "",
+    },
+    4: {
+      prof: "",
+      subject: "",
+    },
+    5: {
+      prof: "",
+      subject: "",
+    },
+    6: {
+      prof: "",
+      subject: "",
+    },
+  },
+  2: {
+    1: {
+      prof: "",
+      subject: "",
+    },
+    2: {
+      prof: "",
+      subject: "",
+    },
+    3: {
+      prof: "",
+      subject: "",
+    },
+    4: {
+      prof: "",
+      subject: "",
+    },
+    5: {
+      prof: "",
+      subject: "",
+    },
+    6: {
+      prof: "",
+      subject: "",
+    },
+  },
+  3: {
+    1: {
+      prof: "",
+      subject: "",
+    },
+    2: {
+      prof: "",
+      subject: "",
+    },
+    3: {
+      prof: "",
+      subject: "",
+    },
+    4: {
+      prof: "",
+      subject: "",
+    },
+    5: {
+      prof: "",
+      subject: "",
+    },
+    6: {
+      prof: "",
+      subject: "",
+    },
+  },
+  4: {
+    1: {
+      prof: "",
+      subject: "",
+    },
+    2: {
+      prof: "",
+      subject: "",
+    },
+    3: {
+      prof: "",
+      subject: "",
+    },
+    4: {
+      prof: "",
+      subject: "",
+    },
+    5: {
+      prof: "",
+      subject: "",
+    },
+    6: {
+      prof: "",
+      subject: "",
+    },
+  },
+  5: {
+    1: {
+      prof: "",
+      subject: "",
+    },
+    2: {
+      prof: "",
+      subject: "",
+    },
+    3: {
+      prof: "",
+      subject: "",
+    },
+    4: {
+      prof: "",
+      subject: "",
+    },
+    5: {
+      prof: "",
+      subject: "",
+    },
+    6: {
+      prof: "",
+      subject: "",
+    },
+  },
+  6: {
+    1: {
+      prof: "",
+      subject: "",
+    },
+    2: {
+      prof: "",
+      subject: "",
+    },
+    3: {
+      prof: "",
+      subject: "",
+    },
+    4: {
+      prof: "",
+      subject: "",
+    },
+    5: {
+      prof: "",
+      subject: "",
+    },
+    6: {
+      prof: "",
+      subject: "",
+    },
+  },
+  7: {
+    1: {
+      prof: "",
+      subject: "",
+    },
+    2: {
+      prof: "",
+      subject: "",
+    },
+    3: {
+      prof: "",
+      subject: "",
+    },
+    4: {
+      prof: "",
+      subject: "",
+    },
+    5: {
+      prof: "",
+      subject: "",
+    },
+    6: {
+      prof: "",
+      subject: "",
+    },
+  },
+};
+
+export { calendar };
